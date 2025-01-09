a3434a4ef46278bb3655e12ad9fea2a18877a359
KhanhDinhh <kiomatnet123@gmail.com>
2025-01-09 21:54:43 +0700

    Update main.dart

--- chating_app/lib/main.dart
+++ chating_app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/introduction_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/main_screen.dart';
+import 'screens/signUp_screen.dart'; // Import màn hình SignUp
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,16 +11,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/sign_up', // Bắt đầu từ màn hình SignUp
       routes: {
         '/': (context) => IntroductionScreen(),
         '/login': (context) => LoginScreen(),
         '/main': (context) => MainScreen(),
+        '/sign_up': (context) => SignUpScreen(), // Thêm màn hình SignUp
       },
     );
   }
