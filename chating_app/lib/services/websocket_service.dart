96f955b45f5b5d3d6a9f2238c6b016a9a7fb67c7
Dlhuutien <Danglehuutien000@gmail.com>
2025-04-14 14:14:47 +0700

    complete emoji chat

--- chating_app/lib/services/websocket_service.dart
+++ chating_app/lib/services/websocket_service.dart
@@ -1,64 +1,3 @@
-// import 'dart:convert';
-// import 'dart:io';
-// import 'package:flutter/foundation.dart';
-//
-// typedef OnMessageReceived = void Function(Map<String, dynamic> message);
-//
-// class WebSocketService {
-//   late WebSocket _socket;
-//   final String userId;
-//   final String chatId;
-//   final OnMessageReceived onMessage;
-//
-//   WebSocketService({
-//     required this.userId,
-//     required this.chatId,
-//     required this.onMessage,
-//   });
-//
-//   Future<void> connect() async {
-//     try {
-//       _socket = await WebSocket.connect("ws://138.2.106.32/ws");
-//
-//       _socket.add(jsonEncode({
-//         "type": "joinSocket",
-//         "userID": userId,
-//       }));
-//
-//       _socket.add(jsonEncode({
-//         "type": "joinChat",
-//         "chatId": chatId,
-//       }));
-//
-//       _socket.listen((data) {
-//         final message = jsonDecode(data);
-//         if (message["type"] == "receiveChat") {
-//           onMessage(message["message"]);
-//         }
-//       });
-//     } catch (e) {
-//       if (kDebugMode) {
-//         print("WebSocketService connection error: $e");
-//       }
-//     }
-//   }
-//
-//   void sendMessage(String content) {
-//     final message = {
-//       "type": "sendChat",
-//       "chatId": chatId,
-//       "messagePayload": {
-//         "type": "text",
-//         "content": content,
-//       }
-//     };
-//     _socket.add(jsonEncode(message));
-//   }
-//
-//   void close() {
-//     _socket.close();
-//   }
-// }
 import 'dart:convert';
 import 'dart:io';
 
@@ -103,22 +42,6 @@ class WebSocketService {
     }
   }
 
-  // void sendMessage(String content) {
-  //   final message = {
-  //     "type": "sendChat",
-  //     "chatId": chatId,
-  //     "messagePayload": {
-  //       "type": "text",
-  //       "content": content,
-  //     },
-  //   };
-  //
-  //   if (_socket != null) {
-  //     _socket!.add(jsonEncode(message));
-  //   } else {
-  //     print("Socket chưa được khởi tạo.");
-  //   }
-  // }
   void sendMessage(String content) {
     if (_socket == null) {
       print("Socket chưa được khởi tạo.");
